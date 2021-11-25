0f9fdb3c3c0841f3a3a89a51e3b902eee94bc927
Praneeth Raj <praneethraj@pop-os.localdomain>
2021-11-25 22:48:31 +0530

    Fix faulty time logic

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var SSE=require("sse")
 var http=require("http")
 var faker=require("faker")
 
-var date=new Date()
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-var val=0
+
 var server=http.createServer(function(req,res){
     res.writeHead(200,{
         "Content-type":"text/event-stream",
         "Access-Control-Allow-Origin":"*"
     })
 setInterval(function(){
+var date=new Date()
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+var val=0
 val++
 time=date
 randomNotification=faker.lorem.sentence();
